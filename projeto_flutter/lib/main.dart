308851f65d15e4941ec50fcbb60ac3fc37c8ac67
renan.okada <rokada@uolinc.com>
2023-10-26 18:45:32 -0300

    feat: Trabalho de Gasolina

--- projeto_flutter/lib/main.dart
+++ projeto_flutter/lib/main.dart
@@ -1,172 +1,82 @@
 import 'package:flutter/material.dart';
 
+double liters = 0;
+double price = 5.00;
+double result = 0;
+List<double> payments = [];
+
 void main() {
-  runApp(TodoApp());
+  runApp(MaterialApp(
+    home: SplashScreen(),
+  ));
 }
 
-class TodoApp extends StatelessWidget {
-  const TodoApp({super.key});
-
+class SplashScreen extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Lista de Tarefas',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const TodoListScreen(),
-    );
-  }
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class TodoListScreen extends StatefulWidget {
-  const TodoListScreen({super.key});
-
+class _SplashScreenState extends State<SplashScreen> {
   @override
-  _TodoListScreenState createState() => _TodoListScreenState();
-}
-
-class _TodoListScreenState extends State<TodoListScreen> {
-  List<TodoItem> todos = [];
-  bool isAddScreenVisible = false;
-
-  void _toggleAddScreen() {
-    setState(() {
-      isAddScreenVisible = !isAddScreenVisible;
-    });
-  }
-
-  void _addTodo() async {
-    final newTodo = await Navigator.of(context).push(
-      PageRouteBuilder(
-        pageBuilder: (context, animation, secondaryAnimation) {
-          return const AddTodoScreen();
-        },
-        transitionsBuilder: (context, animation, secondaryAnimation, child) {
-          const begin = Offset(1.0, 0.0);
-          const end = Offset.zero;
-          const curve = Curves.easeInOut;
-          var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-          var offsetAnimation = animation.drive(tween);
-
-          return SlideTransition(
-            position: offsetAnimation,
-            child: child,
-          );
-        },
-      ),
-    );
-
-    if (newTodo != null) {
-      setState(() {
-        todos.add(TodoItem(newTodo, false));
-      });
-    }
-  }
-
-  void _toggleTodo(int index) {
-    setState(() {
-      todos[index].isCompleted = !todos[index].isCompleted;
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomeScreen()));
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Lista de Tarefas'),
-      ),
-      body: ListView.builder(
-        itemCount: todos.length,
-        itemBuilder: (context, index) {
-          return Dismissible(
-            key: Key(todos[index].task),
-            background: Container(
-              color: Colors.red,
-              alignment: Alignment.centerLeft,
-              child: const Icon(Icons.delete),
-            ),
-            secondaryBackground: Container(
-              color: Colors.green,
-              alignment: Alignment.centerRight,
-              child: const Icon(Icons.check),
-            ),
-            onDismissed: (direction) {
-              if (direction == DismissDirection.startToEnd) {
-                setState(() {
-                  todos.removeAt(index);
-                });
-              } else if (direction == DismissDirection.endToStart) {
-                _toggleTodo(index);
-              }
-            },
-            child: ListTile(
-              title: Text(
-                todos[index].task,
-                style: TextStyle(
-                  decoration: todos[index].isCompleted
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none,
-                  color: todos[index].isCompleted ? Colors.grey : Colors.black,
-                ),
-              ),
-              onTap: () => _toggleTodo(index),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: AnimatedIcon(
-          icon: AnimatedIcons.menu_arrow,
-          progress: isAddScreenVisible
-              ? const AlwaysStoppedAnimation(0.75)
-              : const AlwaysStoppedAnimation(0.0),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/logo1.png', width: 100, height: 100),
+            const SizedBox(height: 20),
+            const Text('Bem-vindo ao Posto de Gasolina'),
+          ],
         ),
-        onPressed: () {
-          if (isAddScreenVisible) {
-            _addTodo();
-          }
-          _toggleAddScreen();
-        },
       ),
     );
   }
 }
 
-class AddTodoScreen extends StatefulWidget {
-  const AddTodoScreen({super.key});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
-  @override
-  _AddTodoScreenState createState() => _AddTodoScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _AddTodoScreenState extends State<AddTodoScreen> {
-  final TextEditingController _textEditingController = TextEditingController();
-
-  void _addTodo() {
-    String newTodo = _textEditingController.text;
-    Navigator.of(context).pop(newTodo);
-  }
-
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Adicionar Tarefa'),
+        title: const Text('Página de Inicio'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: _textEditingController,
-              decoration: const InputDecoration(labelText: 'Tarefa'),
-            ),
-            const SizedBox(height: 16.0),
+            const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: _addTodo,
-              child: const Text('Adicionar'),
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const CalculatorScreen()));
+              },
+              child: const Text('Central de cobrança de combustivel'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const PaymentListScreen()));
+              },
+              child: const Text('Central de pagamentos'),
             ),
           ],
         ),
@@ -175,9 +85,102 @@ class _AddTodoScreenState extends State<AddTodoScreen> {
   }
 }
 
-class TodoItem {
-  String task;
-  bool isCompleted;
+class PaymentListScreen extends StatelessWidget {
+  const PaymentListScreen({super.key});
 
-  TodoItem(this.task, this.isCompleted);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Lista de pagamentos"),
+      ),
+      body: ListView.builder(
+        itemCount: payments.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(
+                'Pagamento $index: R\$ ${payments[index].toStringAsFixed(2)}'),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  const CalculatorScreen({super.key});
+
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+            'Central de cobrança de combustivel: Calculadora de Combustível'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                  labelText: 'Litros de combustível consumidas'),
+              onChanged: (value) {
+                setState(() {
+                  liters = double.tryParse(value) ?? 0;
+                });
+              },
+            ),
+            Text('\n Preço por litro aplicado: R\$ $price'),
+            const SizedBox(height: 20),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => const PaymentConfirmationScreen()));
+              },
+              child: const Text('Confirmar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PaymentConfirmationScreen extends StatelessWidget {
+  const PaymentConfirmationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Confirmação de Pagamento'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                'Total a pagar: R\$ ${liters * price} Por favor, confirme o pagamento.'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                double totalPayment = liters * price;
+                payments.add(totalPayment);
+                Navigator.of(context).pop();
+              },
+              child: const Text('Pagar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
